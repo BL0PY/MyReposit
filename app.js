33fc1353fcca9321a9902e23bb1cee931cab1524
BL0PY <59025804+BL0PY@users.noreply.github.com>
2020-10-10 15:46:14 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,45 @@
 var canvas = document.getElementById("app");
 var ctx = canvas.getContext("2d");
 
-var rect = (x, y, w, h) => {
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-    function move(x, y) {
-        this.x += x;
-        this.y += y;
+class rect{
+    constructor(x, y, w, h){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.vitess = 10;
+    }
+    draw(){
+        ctx.fillStyle = 'white';
+        ctx.fillRect(this.x, this.y, this.w, this.h);
+    }
+    event(x, y, w, h){
+        if(joystick.left()){
+            if(this.x > x){
+                this.x -= this.vitess;
+            }
+        }
+        if(joystick.right()){
+            if(this.x < w-this.w){
+                this.x += this.vitess;
+            }
+        }
+        if(joystick.up()){
+            if(this.y > y){
+                this.y -= this.vitess;
+            }
+        }
+        if(joystick.down()){
+            if(this.y < h-this.h){
+                this.y += this.vitess;
+            }
+        }
     }
 }
 
-var player1 = rect(0, 0, 10, 60);
-var player2 = rect(0, 0, 10, 60);
+var player1 = new rect(400, 450, 10, 60);
+var player2 = new rect(1200, 450, 10, 60);
+var line = new rect(800, 0, 1, 900);
 
 var joystick = new VirtualJoystick({
     mouseSupport: true,
@@ -25,13 +51,15 @@ function affichage() {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, 1600, 900);
     ctx.fillStyle = 'white';
-    ctx.fillRect(player1.x, player1.y, player1.w, player1.h);
-    ctx.fillRect(player2.x, player2.y, player2.w, player2.h);
+    player1.draw();
+    player2.draw();
+    line.draw();
 }
 
 window.main = function () {
     window.requestAnimationFrame(main);
     affichage();
+    player1.event(0, 0, 800, 900);
 };
 
 main();
